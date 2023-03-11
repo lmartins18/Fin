6683c1ace426958eeb17af9ecf5a62f9de98e391
Luis Martins <luismwolf@gmail.com>
2023-03-11 22:17:57 +0000

    progress front-end: added sidenav

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,32 +1,12 @@
-import logo from './logo.svg'
-import {useState} from 'react'
-import {Link, Navigate, Outlet, Route, Routes, useLocation} from "react-router-dom";
-
-import {AnimatePresence} from "framer-motion";
-import IndexPage from "./pages/IndexPage";
-import AuthProvider from "./components/AuthProvider";
-import LoginPage from "./pages/LoginPage";
-import AuthSwitch from "./components/AuthSwitch";
-import LogoutPage from "./pages/LogoutPage";
+import { AnimatePresence } from "framer-motion";
+import Home from "./pages/Home";
 
 function App() {
-    const location = useLocation();
-
-    return (
-        <AuthProvider>
-            <AnimatePresence exitBeforeEnter>
-                <Routes key={location.pathname} location={location}>
-                    <Route path="/" element={
-                        <AuthSwitch
-                            auntificated={<IndexPage/>}
-                            nonAuntificated={<IndexPage/>}/>}
-                    />
-                    <Route path="login" element={<LoginPage/>}/>
-                    <Route path="logout" element={<LogoutPage/>}/>
-                </Routes>
-            </AnimatePresence>
-        </AuthProvider>
-    )
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Home />
+    </AnimatePresence>
+  );
 }
 
-export default App
+export default App;
